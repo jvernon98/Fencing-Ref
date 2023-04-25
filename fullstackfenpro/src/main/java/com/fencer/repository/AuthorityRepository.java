b6eff4d3d468b9a0aad51bacc90044ca88a8f52d
jvernon98 <jvernon@udel.edu>
2023-04-25 15:52:42 -0400

    Initial version of fullstackfenpro generated by generator-jhipster@7.9.3

---
+++ fullstackfenpro/src/main/java/com/fencer/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.fencer.repository;
+
+import com.fencer.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ fullstackfenpro/src/main/java/com/fencer/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.fencer.service.dto;
